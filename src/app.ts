97630c21fbac0d97aeea4021ae82459dcca22734
gensart-x <165239315+gensart-x@users.noreply.github.com>
2024-06-07 16:36:08 +0700

    feat: revertion
    - removing auth functionality
    - removing some views
    - removing controllers

--- src/app.ts
+++ src/app.ts
@@ -21,5 +21,5 @@ app.use('/', indexRouter);
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`⚡Server is running on port: ${PORT}`);
 });
--- src/routes/global.routes.ts
+++ src/routes/global.routes.ts
@@ -1,18 +1,10 @@
 import express from "express";
 import * as mainController from '@controller/main.controller';
-import * as authController from '@controller/auth.controller';
 
 const routes = express.Router();
 
 routes.get('/', mainController.dashboard);
 routes.get('/cookpad', mainController.cookpad)
-routes.get('/pp-couple', _ => { })
-routes.get('/manajemen-api-key', _ => { })
-routes.get('/statistik-api-key', _ => { })
-routes.get('/profil-saya', _ => { })
-
-routes.get('/login', authController.indexLogin)
-routes.get('/daftar', authController.indexRegister)
-routes.get('/logout', _ => { })
+routes.get('/statistik', _ => { })
 
 export default routes
